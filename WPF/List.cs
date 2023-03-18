da3e18714c3ab57ee504864079a770d411f22c13
Niclas Høgh <niclas.hogh@gmail.com>
2023-03-18 21:10:06 +0100

    WPF

---
+++ WPF/List.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices.ActiveDirectory;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF
+{/*
+    public class Person
+    {
+        //Variables & Properties
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+
+        public Person(string firstName, string middleName, string lastName)
+        {
+            this.FirstName = firstName;
+            this.MiddleName = middleName;
+            this.LastName = lastName;
+        }
+    }
+
+    public class Repo
+    {
+        public static List<Person> Load()
+        {
+            //Variables & Properties
+            List<Person> dataList = new List<Person>();
+
+            //Add
+            dataList.Add("","","") ;
+
+            return dataList;
+
+        }
+    }*/
+}
